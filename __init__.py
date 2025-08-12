e5e9eb08f6e30c1fc1636422740852b9f1d22315
Bo Mikkelsen [Netic] <bom@netic.dk>
2025-08-12 12:42:14 +0200

    Initial plugin commit

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from netbox.plugins import PluginConfig
+
+class NetBoxEOXViewsConfig(PluginConfig):
+    name = "netbox_eox_views"
+    verbose_name = "EOX Device Filter"
+    description = "Lists devices with EOX date before today"
+    version = "0.1"
+    author = "Bo Mikkelsen"
+    author_email = "bom@netic.dk"
+    base_url = "eox-views"
+
+config = NetBoxEOXViewsConfig
