764a2f6ab37b3662b97cd8606d3eaa6a23ab8543
Bo Mikkelsen [Netic] <bom@netic.dk>
2025-08-12 14:05:18 +0200

    Initial plugin commit

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='netbox-eox-views',
+    name='netbox_eox_views',
     version='0.1',
     description='A NetBox plugin for EOX device filtering',
     install_requires=[],
-    packages=find_packages(include=["netbox_eox*"]),
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
 )
