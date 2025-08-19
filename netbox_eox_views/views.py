b9ee502af5a9871c4a6ac9b601e69431a7858c27
Bo Mikkelsen [Netic] <bom@netic.dk>
2025-08-19 13:24:07 +0200

    Syntax error

--- netbox_eox_views/views.py
+++ netbox_eox_views/views.py
@@ -1,18 +1,17 @@
 from datetime import date, datetime
 from dcim.models import Device
 from netbox.views import generic
-from django.db.models import Q
 from .tables import LDOSDeviceTable
 
 
 class LDOSDeviceListView(generic.ObjectListView):
     table = LDOSDeviceTable
-    template_name = "dcim/device_list.html"
+    template_name = "device_ldos_list.html"
     action_buttons = ("add",)
 
     def get_queryset(self, request):
         today = date.today()
-        devices = Device.objects.exclude(custom_field_data__ldos_data__isnull=True)
+        devices = Device.objects.filter(Q(status="active") | Q(status="production"))
         matching_ids = []
         for device in devices:
             ldos_value = device.custom_field_data.get("ldos_data")
