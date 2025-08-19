9d820606ca5ade0e8296ee98d451bf8919ef27a2
Bo Mikkelsen [Netic] <bom@netic.dk>
2025-08-19 10:31:07 +0200

    Syntax error

--- netbox_eox_views/urls.py
+++ netbox_eox_views/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import LDOSDeviceListView
 from django.views.generic import RedirectView
 
-url_patterns = [
+urlpatterns = [
     path("", RedirectView.as_view(url="views/", permanent=True)),
     path("views/", LDOSDeviceListView.as_view(), name="ldos-device-list"),
 ]
