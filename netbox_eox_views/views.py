6eb714bfc09b384d142d602a9589b462ba26d8e2
Tim Hansen <tih@netic.dk>
2025-10-27 10:23:40 +0100

    Renamed variables and methods in the expired license view to license expiration terms

--- netbox_eox_views/views.py
+++ netbox_eox_views/views.py
@@ -60,20 +60,20 @@ class LDOSDeviceListView(generic.ObjectListView):
 class ExpiredLicenseDeviceListView(generic.ObjectListView):
     table = ExpiredLicenseDeviceTable
     template_name = "netbox_eox_views/device_list_expired_license.html"
-    action_buttons = ("add", "ldos_year")
+    action_buttons = ("add", "expiry_year")
 
     def get_queryset(self, request):
         today = date.today()
         year_from_today_date = today + relativedelta(years=1)
-        filter_ldos_year = request.GET.get("ldos_year") == "1"
-        ldos_before = request.GET.get("ldos_before")
-        if ldos_before:
+        filter_expiry_year = request.GET.get("expiry_year") == "1"
+        expiry_before = request.GET.get("expiry_before")
+        if expiry_before:
             try:
-                ldos_before_date = datetime.strptime(ldos_before, "%Y-%m-%d").date()
+                expiry_before_date = datetime.strptime(expiry_before, "%Y-%m-%d").date()
             except (ValueError, TypeError):
-                ldos_before_date = None
+                expiry_before_date = None
         else:
-            ldos_before_date = None
+            expiry_before_date = None
 
         devices = Device.objects.filter(Q(status="active") | Q(status="production"))
         matching_ids = []
@@ -85,10 +85,10 @@ class ExpiredLicenseDeviceListView(generic.ObjectListView):
                 contract_end_date = datetime.strptime(service_contract_end, "%Y-%m-%d").date()
             except (ValueError, TypeError):
                 continue
-            if ldos_before_date:
-                if contract_end_date < ldos_before_date:
+            if expiry_before_date:
+                if contract_end_date < expiry_before_date:
                     matching_ids.append(device.id)
-            elif filter_ldos_year:
+            elif filter_expiry_year:
                 if contract_end_date < year_from_today_date:
                     matching_ids.append(device.id)
             else:
@@ -101,7 +101,7 @@ class ExpiredLicenseDeviceListView(generic.ObjectListView):
             "today": date.today()
         }
 
-    def ldos_year_button(self, request):
-        url = f"{reverse('netbox_eox_views:expired_license_device_list')}?ldos_year=1"
+    def expiry_year_button(self, request):
+        url = f"{reverse('netbox_eox_views:expired_license_device_list')}?expiry_year=1"
         return HttpResponseRedirect(url)
 
