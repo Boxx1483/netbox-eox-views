fa2c143823755015f97a7a472a5007515a7a96f7
Tim Hansen <tih@netic.dk>
2025-10-27 13:20:50 +0100

    Navigation changes

--- netbox_eox_views/navigation.py
+++ netbox_eox_views/navigation.py
@@ -1,18 +1,20 @@
-from netbox.plugins import PluginMenu, PluginMenuItem, PluginMenuButton
+from netbox.plugins import PluginMenu, PluginMenuItem
 
 menu = PluginMenu(
-    label="EOX Views",
-    icon_class="mdi mdi-sitemap",
+    label="Device Lifecycle",
+    icon_class="mdi mdi-timer-sand",
     groups=(
-        ("Views", (
+        ("END-OF-LIFE", (
             PluginMenuItem(
                 link="plugins:netbox_eox_views:ldos-device-list",
-                link_text="LDOS Devices",
+                link_text="End-of-Life Devices",
                 permissions=["dcim.view_site", "dcim.view_device"],
             ),
+        )),
+        ("LICENSING", (
             PluginMenuItem(
                 link="plugins:netbox_eox_views:expired-license-device-list",
-                link_text="Expired License Devices",
+                link_text="Expired Licenses",
                 permissions=["dcim.view_site", "dcim.view_device"],
             ),
         )),
